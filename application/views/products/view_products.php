5bc5e67593041a2fb38e0a5c5e3f556070af35b5
VirginiaNdungu <vwairimu@virtualcity.co.ke>
2017-01-23 15:55:27 +0300

    Revamp products interface

--- application/views/products/view_products.php
+++ application/views/products/view_products.php
@@ -1,41 +1,35 @@
-<body >
-<!--Header Section-->
-<?Php $this->load->view('templates/app_header');?>
-
-<!--Navigation Section-->
-<?Php
-if( $user_role === 'ADMIN'){
-    $this->load->view('templates/navigation');
-}else if($user_role === 'SUPER_USER'){
-    $this->load->view('templates/navigation_super_user');
-}else{
-    $this->load->view('templates/navigation_user');
-}
-?>
-
-<div id="content" class="no-sidebar"> <!-- Content start -->
-    <div class="top_bar">
-        <ul class="breadcrumb">
-            <li><a href="<?=base_url('dashboard')?>"><i class="icon-home"></i> Home</a> <span class="divider">/</span></li>
-            <li class="active"><a>Products</a></li>
-        </ul>
+<!-- begin #content -->
+<div id="content" class="content">
+ 
+    <div class="breadcrumb-container ">
+        <ol class="breadcrumb pull-left ">
+        <li><a href="javascript:;"><i class="fa fa-home"></i> Home</a></li>
+        <li><a href="javascript:;"><i class="fa fa-file"></i> Products</a></li>
+        <li class="active">View Products</li>
+    </ol>
     </div>
-    <div class="inner_content">
-        <div id="alert_placeholder">
-            <?php
-            $appmsg = $this->session->flashdata('appmsg');
-            if(!empty($appmsg)){ ?>
-                <div id="alertdiv" class="alert <?=$this->session->flashdata('alert_type') ?> "><a class="close" data-dismiss="alert">x</a><span><?= $appmsg ?></span></div>
-            <?php } ?>
-        </div>
-        <div class="widgets_area">
+ 
+ <div id="alert_placeholder">
+        <?php
+        $appmsg = $this->session->flashdata('appmsg');
+        if(!empty($appmsg)){ ?>
+        <div id="alertdiv" class="alert <?=$this->session->flashdata('alert_type') ?> "><a class="close" data-dismiss="alert">x</a><span><?= $appmsg ?></span></div>
+        <?php } ?>
+    </div>
 
-            <div class="well blue">
-                <div class="well-header">
-                    <h5>View Products</h5>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-no-rounded-corner panel-default">
+                <div class="panel-heading">
+                    <div class="panel-heading-btn">
+                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand"><i class="fa fa-expand"></i></a>
+                        <a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse"><i class="fa fa-minus"></i></a>
+                         
+                    </div>
+                    <h4 class="panel-title">View Products</h4>
                 </div>
-                <div class="well-content no_search">
-
+                <div class="panel-body">
+                   
                     <table class="table table-striped table-bordered table-hover datatable"  id="example">
                         <thead>
                         <tr>
@@ -52,22 +46,15 @@ if( $user_role === 'ADMIN'){
                         </thead>
 
                     </table>
-
+                   
                 </div>
+                <div class="panel-footer">View Products</div>
             </div>
-
         </div>
     </div>
 </div>
-
-
-<!-- Bootstrap core JavaScript
-================================================== -->
-<!-- Placed at the end of the document so the pages load faster -->
-
-<script src="<?= base_url('assets/js/jquery-1.11.1.js'); ?>"></script>
-<script src="<?= base_url('assets/js/jquery.dataTables.min.js'); ?>"></script>
-<script type="text/javascript" src="<?php echo base_url('assets/tabletools/js/datatables.tableTools.js'); ?>"></script>
+<!-- end #content -->
+<!--Header Section-->
 <script type="text/javascript">
     jQuery(document).ready(function(){
         jQuery('#example').dataTable({
@@ -108,19 +95,3 @@ if( $user_role === 'ADMIN'){
     });
 
 </script>
-<script src="<?php echo base_url('assets/js/jquery-ui-1.10.3.js'); ?>"></script>
-<script src="<?php echo base_url('assets/js/bootstrap.js'); ?>"></script>
-<script src="<?php echo base_url('assets/datatables/js/responsive.js'); ?>"></script>
-
-<script src="<?php echo  base_url('assets/js/library/jquery.collapsible.min.js'); ?>"></script>
-<script src="<?php echo  base_url('assets/js/library/jquery.mCustomScrollbar.min.js'); ?>"></script>
-<script src="<?php echo  base_url('assets/js/library/jquery.mousewheel.min.js'); ?>"></script>
-<script src="<?php echo  base_url('assets/js/library/jquery.uniform.min.js'); ?>"></script>
-
-<script src="<?php echo  base_url('assets/js/library/jquery.autosize-min.js'); ?>"></script>
-
-
-<script src="<?php echo base_url('assets/js/design_core.js'); ?>"></script>
-
-</body>
-</html>
