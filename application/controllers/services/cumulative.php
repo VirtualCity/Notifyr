211ff5fc552c4afeeabd03eb1b26fbe4343cf49d
Charles Wahome <thewahome.cw@gmail.com>
2017-02-22 10:36:24 +0300

    receive cummulative reports and send SMS

--- application/controllers/services/cumulative.php
+++ application/controllers/services/cumulative.php
@@ -11,6 +11,9 @@ class Cumulative extends CI_Controller{
 
     function __construct(){
         parent::__construct();
+        $this->load->library('sms/SmsSender.php');
+        $this->load->model('sms_model');
+        $this->load->model('sendsms_model');
     }
 
     function index(){
@@ -33,4 +36,64 @@ class Cumulative extends CI_Controller{
 
     }
 
-} 
+    public function ReceiveCumulativeData()
+    {
+        $cumulativeReport = json_decode(file_get_contents('php://input'), true);
+        foreach ($cumulativeReport as $report) {
+            $statusCode= $report['StatusCode'];
+            $statusDetail = $report['StatusDetail'];
+            $farmerCode = $report['Code'];
+            $msisdn = $this->ChangePhoneNumberFormat($report['MobileNumber']);
+            $farmerName = $report['Name'];
+            $asAtDate = $report['Date'];
+            $summaryDetail = $report['Deliveries'];
+            $message=$farmerName.', Supplier Code: '.$farmerCode.', ';
+            if(sizeof($summaryDetail) > 0){
+                log_message('info','Summary detail is not null');
+                foreach ($summaryDetail as $commodity){
+                    $message = $message. ' '.$commodity['Commodity'].':'.$commodity['Weight'].'Kg ';
+                }
+            }else{
+                log_message('info','Summary detail is null');
+                $message = $message.' Cumulative Weight: 0 Kg ';
+            }
+            $message = $message.'as at '.$asAtDate;
+            $recipients= array('tel:'.$msisdn);
+            $msg_sent= $this->sendsms_model->send_sms($recipients,$message);
+            if($msg_sent=='success'){
+                $this->sms_model->save_sms($msisdn,"Cumulative Report",$message,0);
+
+            }else{
+                echo 'Message to '.$msisdn.' failed.';
+                log_message('appmsg', 'Message to '.$msisdn.' failed.');
+            }
+            echo $msisdn;
+        }
+    }
+
+    public function ChangePhoneNumberFormat($phoneNumber){
+        $phoneNumber = preg_replace('/[^0-9]/','',$phoneNumber);
+
+        if(strlen($phoneNumber) == 12) {
+            $countryCode = substr($phoneNumber, 0, strlen($phoneNumber)-9);
+            $areaCode = substr($phoneNumber, -8, 4);
+            $nextThree = substr($phoneNumber, -9, 3);
+            $lastSix = substr($phoneNumber, -6, 6);
+            $phoneNumber = $countryCode.$nextThree.$lastSix;
+        }
+        else if(strlen($phoneNumber) == 10) {
+            $nextThree = substr($phoneNumber, 1, 3);
+            $lastFour = substr($phoneNumber, 4, 6);
+            $phoneNumber = '254'.$nextThree.$lastFour;
+        }
+        else if(strlen($phoneNumber) == 9) {
+            $nextThree = substr($phoneNumber, 0, 3);
+            $lastFour = substr($phoneNumber, 3, 6);
+            $phoneNumber = '254'.$nextThree.''.$lastFour;
+        }
+        return $phoneNumber;
+    }
+
+
+
+}
